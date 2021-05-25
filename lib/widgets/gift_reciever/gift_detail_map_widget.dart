7c634f89bfe3267ac05f0873872e90103f7a3891
Mahbubur Rahman <rifat0153@gmail.com>
2021-05-25 17:59:23 +0600

    GiftDetailView complete

--- lib/widgets/gift_reciever/gift_detail_map_widget.dart
+++ lib/widgets/gift_reciever/gift_detail_map_widget.dart
@@ -3,6 +3,7 @@ import 'dart:async';
 import 'package:alokito_new/controller/auth/auth_controller.dart';
 import 'package:alokito_new/controller/gift/gift_controller.dart';
 import 'package:alokito_new/models/gift_giver/gift_giver.dart';
+import 'package:alokito_new/shared/config.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -26,54 +27,66 @@ class GiftDetailMapWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     var distanceOfGift =
         authController.calculateDistanceForGiftDetail(giftGiver: giftGiver);
+    var giftType = giftController.convertGiftType(giftGiver.giftType);
 
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  'Pick up Location',
-                  style: TextStyle(fontWeight: FontWeight.bold),
-                ),
-                Text('$distanceOfGift km away'),
-              ],
-            ),
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'Pick up Location',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text('$distanceOfGift km away'),
+            ],
           ),
-          Container(
-            height: 200,
-            width: double.infinity,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ClipRRect(
-                child: GoogleMap(
-                  mapType: MapType.normal,
-                  onMapCreated: (GoogleMapController controller) {
-                    _controller.complete(controller);
-                  },
-                  initialCameraPosition: CameraPosition(
-                    target: LatLng(giftGiver.position.geopoint.latitude,
-                        giftGiver.position.geopoint.longitude),
-                    zoom: 15,
+        ),
+        Container(
+          color: GIFT_ADD_FORM_COLOR,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Align(
+                child: Text('${giftGiver.location}, ${giftGiver.area} '),
+              ),
+              Container(
+                height: 200,
+                width: double.infinity,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: ClipRRect(
+                    child: Card(
+                      elevation: 20,
+                      child: GoogleMap(
+                        mapType: MapType.normal,
+                        onMapCreated: (GoogleMapController controller) {
+                          _controller.complete(controller);
+                        },
+                        initialCameraPosition: CameraPosition(
+                          target: LatLng(giftGiver.position.geopoint.latitude,
+                              giftGiver.position.geopoint.longitude),
+                          zoom: 15,
+                        ),
+                        markers: [
+                          Marker(
+                            markerId: MarkerId('1'),
+                            position: LatLng(
+                                giftGiver.position.geopoint.latitude,
+                                giftGiver.position.geopoint.longitude),
+                          )
+                        ].toSet(),
+                      ),
+                    ),
                   ),
-                  markers: [
-                    Marker(
-                      markerId: MarkerId('1'),
-                      position: LatLng(giftGiver.position.geopoint.latitude,
-                          giftGiver.position.geopoint.longitude),
-                    )
-                  ].toSet(),
                 ),
               ),
-            ),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
