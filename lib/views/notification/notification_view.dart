498f7b0746591f419159932c67d65617ca424f70
Mahbubur Rahman <rifat0153@gmail.com>
2021-06-03 17:37:42 +0600

    request notif

--- lib/views/notification/notification_view.dart
+++ lib/views/notification/notification_view.dart
@@ -2,6 +2,7 @@ import 'package:alokito_new/controller/auth/auth_controller.dart';
 import 'package:alokito_new/controller/gift/gift_notification_controller.dart';
 import 'package:alokito_new/models/my_enums.dart';
 import 'package:alokito_new/models/notification/gift_notification.dart';
+import 'package:alokito_new/views/notification/notif_requester_details_view.dart';
 import 'package:alokito_new/views/notification/notification_details_view.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -88,7 +89,14 @@ class NotificationListWidget extends StatelessWidget {
                 index: i,
               );
             } else {
-              Container();
+              return GiftConfirmedRequesterWidget(
+                key: ValueKey(
+                    giftNotificationController.giftNotificationList[i].id),
+                giftNotificationController: giftNotificationController,
+                giftNotification:
+                    giftNotificationController.giftNotificationList[i],
+                index: i,
+              );
             }
           }
           return Center(child: Text('No Notifications'));
@@ -235,6 +243,86 @@ class GiftConfirmedWidget extends StatelessWidget {
                           children: <TextSpan>[
                             TextSpan(
                                 text: '${giftNotification.requesterName}',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold)),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text('$difference hours ago'),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class GiftConfirmedRequesterWidget extends StatelessWidget {
+  const GiftConfirmedRequesterWidget(
+      {required Key key,
+      required this.giftNotificationController,
+      required this.giftNotification,
+      required this.index})
+      : super(key: key);
+
+  final int index;
+  final GiftNotification giftNotification;
+  final GiftNotificationController giftNotificationController;
+
+  @override
+  Widget build(BuildContext context) {
+    print('uid is:   ' + giftNotification.requesterUid);
+    print('current user uid:   ' + FirebaseAuth.instance.currentUser!.uid);
+
+    var giftType = convertGiftType(giftNotification.giftType);
+    var date = DateTime.now();
+    var notificationCreatedAt = DateTime.fromMillisecondsSinceEpoch(
+        giftNotification.createdAt.millisecondsSinceEpoch);
+    var difference = date.difference(notificationCreatedAt).inHours;
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: GestureDetector(
+        onTap: () {
+          Get.to(() => NotifRequesterDetailsView());
+          giftNotificationController.notificationIndex.value = index;
+        },
+        child: Card(
+          child: Row(
+            children: [
+              Flexible(
+                flex: 1,
+                child: Image.network(
+                  giftNotification.giftImageUrl,
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Flexible(
+                flex: 7,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: RichText(
+                        text: TextSpan(
+                          text: 'Your gift request for ',
+                          style: DefaultTextStyle.of(context).style,
+                          children: <TextSpan>[
+                            TextSpan(
+                                text: '$giftType',
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold)),
+                            const TextSpan(text: ' was confrimed by '),
+                            TextSpan(
+                                text: '${giftNotification.giverName}',
                                 style: const TextStyle(
                                     fontWeight: FontWeight.bold)),
                           ],
