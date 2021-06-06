3b63631d056b986efc28e182ce2884fd6f11b7a4
Mahbubur Rahman <rifat0153@gmail.com>
2021-06-06 14:43:45 +0600

    Requestger NOtif details view

--- lib/views/notification/notif_requester_details_view.dart
+++ lib/views/notification/notif_requester_details_view.dart
@@ -53,7 +53,7 @@ class NotifRequesterDetailsView extends StatelessWidget {
           ),
           child: Column(
             children: [
-              SizedBox(height: 50),
+              SizedBox(height: 60),
               Expanded(
                 child: Container(
                   // height: Get.height - ,
@@ -61,78 +61,10 @@ class NotifRequesterDetailsView extends StatelessWidget {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        SizedBox(height: Get.height * 0.1),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(horizontal: 20),
-                              child: Container(
-                                width: Get.width - 40,
-                                color: GIFT_ADD_FORM_COLOR,
-                                padding: const EdgeInsets.symmetric(
-                                    horizontal: 16, vertical: 8),
-                                child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text('Gift'),
-                                    const SizedBox(height: 8),
-                                    Text(
-                                      giftType,
-                                      style:
-                                          boldFontStyle.copyWith(fontSize: 25),
-                                    ),
-                                    const SizedBox(height: 8),
-                                    Text(
-                                      'Offered By',
-                                      style: boldFontStyle,
-                                    ),
-                                    const SizedBox(height: 8),
-                                    UserDetailWidget(
-                                      imageUrl: giftNotification.giverImageUrl,
-                                      userJoinedAt:
-                                          giftNotification.giverJoinedAt,
-                                      userName: giftNotification.giverName,
-                                    ),
-                                    UserLocationWidget(
-                                      giftNotification: giftNotification,
-                                      rating: giftNotification.giverAvgRating
-                                          .toInt(),
-                                      lat: giftNotification
-                                          .giverPosition.geopoint.latitude,
-                                      lng: giftNotification
-                                          .giverPosition.geopoint.longitude,
-                                    ),
-                                    SizedBox(height: 16),
-                                    Text(
-                                      'Requested By',
-                                      style: boldFontStyle,
-                                    ),
-                                    const SizedBox(height: 8),
-                                    UserDetailWidget(
-                                      imageUrl:
-                                          giftNotification.requesterImageUrl,
-                                      userJoinedAt:
-                                          giftNotification.requesterJoinedAt,
-                                      userName: giftNotification.requesterName,
-                                    ),
-                                    UserLocationWidget(
-                                      giftNotification: giftNotification,
-                                      rating: giftNotification
-                                          .requesterAvgRating
-                                          .toInt(),
-                                      lat: giftNotification
-                                          .requesterPosition.geopoint.latitude,
-                                      lng: giftNotification
-                                          .requesterPosition.geopoint.longitude,
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
+                        const SizedBox(height: 8),
+                        _UserAndGiverDetails(
+                            giftType: giftType,
+                            giftNotification: giftNotification),
                         StyledContainer(
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
@@ -155,69 +87,17 @@ class NotifRequesterDetailsView extends StatelessWidget {
                             ),
                           ),
                         ),
-                        StyledContainer(
-                          child: Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                Container(
-                                  height: 60,
-                                  width: 75,
-                                  decoration: BoxDecoration(
-                                    color: GIFT_ADD_FORM_SUBMIT,
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 5),
-                                    child: Column(
-                                      children: [
-                                        Transform.rotate(
-                                          angle: -math.pi / 4,
-                                          child: const FaIcon(
-                                            FontAwesomeIcons.phoneVolume,
-                                            color: Colors.white,
-                                            size: 30,
-                                          ),
-                                        ),
-                                        Text('Voice Call',
-                                            style: whiteFontStyle.copyWith(
-                                                fontSize: 10))
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  height: 60,
-                                  width: 75,
-                                  decoration: BoxDecoration(
-                                    color: GIFT_ADD_FORM_SUBMIT,
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  child: Padding(
-                                    padding: EdgeInsets.symmetric(vertical: 5),
-                                    child: Column(
-                                      children: [
-                                        const FaIcon(
-                                          FontAwesomeIcons.commentDots,
-                                          color: Colors.white,
-                                          size: 30,
-                                        ),
-                                        Text('Conversation',
-                                            style: whiteFontStyle.copyWith(
-                                                fontSize: 10))
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
+                        _ContactWidget(),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(vertical: 20),
+                          child: Text('Status', style: boldFontStyle),
                         ),
-                        Text('Status', style: boldFontStyle),
-                        Text('On the way',
-                            style: boldFontStyle.copyWith(fontSize: 30)),
-                        _GuideLines(),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 16),
+                          child: Text('On the way',
+                              style: boldFontStyle.copyWith(fontSize: 30)),
+                        ),
+                        const _GuideLines(),
                       ],
                     ),
                   ),
@@ -231,22 +111,192 @@ class NotifRequesterDetailsView extends StatelessWidget {
   }
 }
 
+class _ContactWidget extends StatelessWidget {
+  const _ContactWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return StyledContainer(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 60,
+              width: 75,
+              decoration: BoxDecoration(
+                color: GIFT_ADD_FORM_SUBMIT,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  children: [
+                    Transform.rotate(
+                      angle: -math.pi / 4,
+                      child: const FaIcon(
+                        FontAwesomeIcons.phoneVolume,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                    ),
+                    Text('Voice Call',
+                        style: whiteFontStyle.copyWith(fontSize: 10))
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 60,
+              width: 75,
+              decoration: BoxDecoration(
+                color: GIFT_ADD_FORM_SUBMIT,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 5),
+                child: Column(
+                  children: [
+                    const FaIcon(
+                      FontAwesomeIcons.commentDots,
+                      color: Colors.white,
+                      size: 30,
+                    ),
+                    Text('Conversation',
+                        style: whiteFontStyle.copyWith(fontSize: 10))
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _UserAndGiverDetails extends StatelessWidget {
+  const _UserAndGiverDetails({
+    Key? key,
+    required this.giftType,
+    required this.giftNotification,
+  }) : super(key: key);
+
+  final String giftType;
+  final GiftNotification giftNotification;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Container(
+            width: Get.width - 40,
+            color: GIFT_ADD_FORM_COLOR,
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text('Gift'),
+                const SizedBox(height: 8),
+                Text(
+                  giftType,
+                  style: boldFontStyle.copyWith(fontSize: 25),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  'Offered By',
+                  style: boldFontStyle,
+                ),
+                const SizedBox(height: 8),
+                UserDetailWidget(
+                  imageUrl: giftNotification.giverImageUrl,
+                  userJoinedAt: giftNotification.giverJoinedAt,
+                  userName: giftNotification.giverName,
+                ),
+                UserLocationWidget(
+                  giftNotification: giftNotification,
+                  rating: giftNotification.giverAvgRating.toInt(),
+                  lat: giftNotification.giverPosition.geopoint.latitude,
+                  lng: giftNotification.giverPosition.geopoint.longitude,
+                ),
+                SizedBox(height: 16),
+                Text(
+                  'Requested By',
+                  style: boldFontStyle,
+                ),
+                const SizedBox(height: 8),
+                UserDetailWidget(
+                  imageUrl: giftNotification.requesterImageUrl,
+                  userJoinedAt: giftNotification.requesterJoinedAt,
+                  userName: giftNotification.requesterName,
+                ),
+                UserLocationWidget(
+                  giftNotification: giftNotification,
+                  rating: giftNotification.requesterAvgRating.toInt(),
+                  lat: giftNotification.requesterPosition.geopoint.latitude,
+                  lng: giftNotification.requesterPosition.geopoint.longitude,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
 class _GuideLines extends StatelessWidget {
   const _GuideLines({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Icon(FontAwesomeIcons.bullhorn),
-        Column(
-          children: [
-            Text('* Please see the '),
-            Text('data'),
-            Text('data'),
-          ],
-        )
-      ],
+    return Container(
+      // width: Get.width,
+      child: Row(
+        children: [
+          SizedBox(width: 16),
+          Icon(FontAwesomeIcons.bullhorn),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        const TextSpan(text: '* Please see the '),
+                        TextSpan(
+                          text: 'Guideline for Food Adding',
+                          style: boldFontStyle.copyWith(color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text('data'),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(text: '* Request to follow '),
+                        TextSpan(
+                          text: 'Covid - 19: Sharing & Safety Guidelines',
+                          style: boldFontStyle.copyWith(color: Colors.red),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
