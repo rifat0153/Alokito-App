64864ce46432f1b47c64c3b45dae64b330db4366
Mahbubur Rahman <rifat0153@gmail.com>
2021-06-06 13:41:32 +0600

    gift Notfi details

--- lib/views/notification/notif_requester_details_view.dart
+++ lib/views/notification/notif_requester_details_view.dart
@@ -51,159 +51,179 @@ class NotifRequesterDetailsView extends StatelessWidget {
                 image: AssetImage('assets/images/gift_details.png'),
                 fit: BoxFit.fill),
           ),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                SizedBox(height: Get.height * 0.1),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 20),
-                      child: Container(
-                        width: Get.width - 40,
-                        color: GIFT_ADD_FORM_COLOR,
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 16, vertical: 8),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Expanded(
+                child: Container(
+                  // height: Get.height - ,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        SizedBox(height: Get.height * 0.1),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                            Text('Gift'),
-                            const SizedBox(height: 8),
-                            Text(
-                              giftType,
-                              style: boldFontStyle.copyWith(fontSize: 25),
-                            ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Offered By',
-                              style: boldFontStyle,
-                            ),
-                            const SizedBox(height: 8),
-                            UserDetailWidget(
-                              imageUrl: giftNotification.giverImageUrl,
-                              userJoinedAt: giftNotification.giverJoinedAt,
-                              userName: giftNotification.giverName,
-                            ),
-                            UserLocationWidget(
-                              giftNotification: giftNotification,
-                              rating: giftNotification.giverAvgRating.toInt(),
-                              lat: giftNotification
-                                  .giverPosition.geopoint.latitude,
-                              lng: giftNotification
-                                  .giverPosition.geopoint.longitude,
-                            ),
-                            SizedBox(height: 16),
-                            Text(
-                              'Requested By',
-                              style: boldFontStyle,
-                            ),
-                            const SizedBox(height: 8),
-                            UserDetailWidget(
-                              imageUrl: giftNotification.requesterImageUrl,
-                              userJoinedAt: giftNotification.requesterJoinedAt,
-                              userName: giftNotification.requesterName,
-                            ),
-                            UserLocationWidget(
-                              giftNotification: giftNotification,
-                              rating:
-                                  giftNotification.requesterAvgRating.toInt(),
-                              lat: giftNotification
-                                  .requesterPosition.geopoint.latitude,
-                              lng: giftNotification
-                                  .requesterPosition.geopoint.longitude,
+                            Padding(
+                              padding:
+                                  const EdgeInsets.symmetric(horizontal: 20),
+                              child: Container(
+                                width: Get.width - 40,
+                                color: GIFT_ADD_FORM_COLOR,
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 16, vertical: 8),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text('Gift'),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      giftType,
+                                      style:
+                                          boldFontStyle.copyWith(fontSize: 25),
+                                    ),
+                                    const SizedBox(height: 8),
+                                    Text(
+                                      'Offered By',
+                                      style: boldFontStyle,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    UserDetailWidget(
+                                      imageUrl: giftNotification.giverImageUrl,
+                                      userJoinedAt:
+                                          giftNotification.giverJoinedAt,
+                                      userName: giftNotification.giverName,
+                                    ),
+                                    UserLocationWidget(
+                                      giftNotification: giftNotification,
+                                      rating: giftNotification.giverAvgRating
+                                          .toInt(),
+                                      lat: giftNotification
+                                          .giverPosition.geopoint.latitude,
+                                      lng: giftNotification
+                                          .giverPosition.geopoint.longitude,
+                                    ),
+                                    SizedBox(height: 16),
+                                    Text(
+                                      'Requested By',
+                                      style: boldFontStyle,
+                                    ),
+                                    const SizedBox(height: 8),
+                                    UserDetailWidget(
+                                      imageUrl:
+                                          giftNotification.requesterImageUrl,
+                                      userJoinedAt:
+                                          giftNotification.requesterJoinedAt,
+                                      userName: giftNotification.requesterName,
+                                    ),
+                                    UserLocationWidget(
+                                      giftNotification: giftNotification,
+                                      rating: giftNotification
+                                          .requesterAvgRating
+                                          .toInt(),
+                                      lat: giftNotification
+                                          .requesterPosition.geopoint.latitude,
+                                      lng: giftNotification
+                                          .requesterPosition.geopoint.longitude,
+                                    ),
+                                  ],
+                                ),
+                              ),
                             ),
                           ],
                         ),
-                      ),
-                    ),
-                  ],
-                ),
-                StyledContainer(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      LocationWidget(giftNotification: giftNotification),
-                    ],
-                  ),
-                ),
-                StyledContainer(
-                  child: NotifMapWidget(giftNotification: giftNotification),
-                ),
-                StyledContainer(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 20),
-                    child: Text(
-                      'Contact this person',
-                      style: boldFontStyle,
-                    ),
-                  ),
-                ),
-                StyledContainer(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 8),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: [
-                        Container(
-                          height: 60,
-                          width: 75,
-                          decoration: BoxDecoration(
-                            color: GIFT_ADD_FORM_SUBMIT,
-                            borderRadius: BorderRadius.circular(10),
+                        StyledContainer(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              LocationWidget(
+                                  giftNotification: giftNotification),
+                            ],
                           ),
+                        ),
+                        StyledContainer(
+                          child: NotifMapWidget(
+                              giftNotification: giftNotification),
+                        ),
+                        StyledContainer(
                           child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: Column(
+                            padding: const EdgeInsets.symmetric(vertical: 20),
+                            child: Text(
+                              'Contact this person',
+                              style: boldFontStyle,
+                            ),
+                          ),
+                        ),
+                        StyledContainer(
+                          child: Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 8),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
                               children: [
-                                Transform.rotate(
-                                  angle: -math.pi / 4,
-                                  child: const FaIcon(
-                                    FontAwesomeIcons.phoneVolume,
-                                    color: Colors.white,
-                                    size: 30,
+                                Container(
+                                  height: 60,
+                                  width: 75,
+                                  decoration: BoxDecoration(
+                                    color: GIFT_ADD_FORM_SUBMIT,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 5),
+                                    child: Column(
+                                      children: [
+                                        Transform.rotate(
+                                          angle: -math.pi / 4,
+                                          child: const FaIcon(
+                                            FontAwesomeIcons.phoneVolume,
+                                            color: Colors.white,
+                                            size: 30,
+                                          ),
+                                        ),
+                                        Text('Voice Call',
+                                            style: whiteFontStyle.copyWith(
+                                                fontSize: 10))
+                                      ],
+                                    ),
                                   ),
                                 ),
-                                Text('Voice Call',
-                                    style:
-                                        whiteFontStyle.copyWith(fontSize: 10))
-                              ],
-                            ),
-                          ),
-                        ),
-                        Container(
-                          height: 60,
-                          width: 75,
-                          decoration: BoxDecoration(
-                            color: GIFT_ADD_FORM_SUBMIT,
-                            borderRadius: BorderRadius.circular(10),
-                          ),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: 5),
-                            child: Column(
-                              children: [
-                                const FaIcon(
-                                  FontAwesomeIcons.commentDots,
-                                  color: Colors.white,
-                                  size: 30,
+                                Container(
+                                  height: 60,
+                                  width: 75,
+                                  decoration: BoxDecoration(
+                                    color: GIFT_ADD_FORM_SUBMIT,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Padding(
+                                    padding: EdgeInsets.symmetric(vertical: 5),
+                                    child: Column(
+                                      children: [
+                                        const FaIcon(
+                                          FontAwesomeIcons.commentDots,
+                                          color: Colors.white,
+                                          size: 30,
+                                        ),
+                                        Text('Conversation',
+                                            style: whiteFontStyle.copyWith(
+                                                fontSize: 10))
+                                      ],
+                                    ),
+                                  ),
                                 ),
-                                Text('Conversation',
-                                    style:
-                                        whiteFontStyle.copyWith(fontSize: 10))
                               ],
                             ),
                           ),
                         ),
+                        Text('Status', style: boldFontStyle),
+                        Text('On the way',
+                            style: boldFontStyle.copyWith(fontSize: 30)),
+                        _GuideLines(),
                       ],
                     ),
                   ),
                 ),
-                Text('Status', style: boldFontStyle),
-                Text('On the way', style: boldFontStyle.copyWith(fontSize: 30)),
-                _GuideLines(),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
