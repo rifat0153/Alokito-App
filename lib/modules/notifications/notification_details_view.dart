27748a8310b7dcecb45f0fe107c355fb447f7628
Mahbubur Rahman <rifat0153@gmail.com>
2021-06-09 18:11:48 +0600

    Gift done init notif view

--- lib/modules/notifications/notification_details_view.dart
+++ lib/modules/notifications/notification_details_view.dart
@@ -6,6 +6,7 @@ import 'package:alokito_new/models/notification/gift_notification.dart';
 import 'package:alokito_new/shared/config.dart';
 import 'package:alokito_new/shared/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:line_icons/line_icon.dart';
 
@@ -97,6 +98,70 @@ class NotificationDetailsView extends StatelessWidget {
   }
 }
 
+class _GuideLines extends StatelessWidget {
+  const _GuideLines({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // width: Get.width,
+      child: Row(
+        children: [
+          const SizedBox(width: 16),
+          const Icon(FontAwesomeIcons.bullhorn),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '* Please see the ',
+                          style: whiteFontStyle.copyWith(
+                              fontSize: notificationFontSize,
+                              color: Colors.black),
+                        ),
+                        TextSpan(
+                          text: 'Guideline for Food Adding',
+                          style: boldFontStyle.copyWith(
+                              color: Colors.red,
+                              fontSize: notificationFontSize),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: '* Request to follow ',
+                          style: whiteFontStyle.copyWith(
+                              fontSize: notificationFontSize,
+                              color: Colors.black),
+                        ),
+                        TextSpan(
+                          text: 'Covid - 19: Sharing & Safety Guidelines',
+                          style: boldFontStyle.copyWith(
+                            color: Colors.red,
+                            fontSize: notificationFontSize,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
 class DecistionWidget extends StatelessWidget {
   DecistionWidget({required this.giftNotification});
   final GiftNotification giftNotification;
@@ -182,7 +247,8 @@ class DecistionWidget extends StatelessWidget {
                                       giftNotification: giftNotification);
                               giftNotificationController.giftNotificationService
                                   .changeRequestStatus(
-                                    giftRequestStatus: GiftRequestStatus.requestConfirmed,
+                                giftRequestStatus:
+                                    GiftRequestStatus.requestConfirmed,
                                 giftRequestStatusForGiver:
                                     GiftRequestStatus.requestConfirmed,
                                 giftRequestStatusForRequester:
