d3be801eb952ebbde71fe78eb32bc8ecf93f8bcb
Mahbubur Rahman <rifat0153@gmail.com>
2021-06-22 19:52:58 +0600

    GiftAskRequqestView appbar fixed

--- lib/modules/gift_ask/gift_ask_request_view.dart
+++ lib/modules/gift_ask/gift_ask_request_view.dart
@@ -1,6 +1,7 @@
 import 'package:alokito_new/modules/gift_ask/widgets/gift_ask_map_widget.dart';
 import 'package:alokito_new/modules/map/my_map_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class GiftAskRequestView extends StatelessWidget {
   const GiftAskRequestView({Key? key}) : super(key: key);
@@ -9,10 +10,25 @@ class GiftAskRequestView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 5,
-        foregroundColor: Colors.grey,
-        backgroundColor: Colors.grey[400],
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+              icon: const Icon(
+                Icons.cancel,
+                color: Colors.black,
+              ),
+              onPressed: () => Get.back())
+        ],
+        title: Text(
+          'My Request',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        backgroundColor: Colors.grey[200],
+        shadowColor: Colors.white,
       ),
+
       body: GiftAskMapWidget(),
       // body: MyMapView(),
     );
