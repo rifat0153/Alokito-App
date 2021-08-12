43f66d9fa9e36c7799d7952451ca0ae3d65f8976
Mahbubur Rahman <rifat0153@gmail.com>
2021-08-12 20:11:05 +0600

    SharedPref added and local language saved

--- lib/modules/language/language_controller.dart
+++ lib/modules/language/language_controller.dart
@@ -1,10 +1,42 @@
 import 'dart:ui';
 import 'package:get/get.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LanguageController extends GetxController {
-    
-  void changeLocale(String language, String country) {
+  // @override
+  // void onInit() async {
+  //   super.onInit();
+  //   await setSavedLocal();
+  // }
+
+  @override
+  void onReady() {
+    super.onReady();
+    setSavedLocal();
+  }
+
+  Future<void> setSavedLocal() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
+    final String languageCode = prefs.getString('languageCode') ?? 'en';
+    final String countryCode = prefs.getString('countryCode') ?? 'US';
+
+    // print('IN setSavedLocal');
+    // print(languageCode);
+    // print(countryCode);
+
+    await changeLocale(languageCode, countryCode);
+  }
+
+  Future<void> changeLocale(String language, String country) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+
     final locale = Locale(language, country);
+
+    // print('Change locale called with $language $country');
+
     Get.updateLocale(locale);
+    await prefs.setString('languageCode', language);
+    await prefs.setString('countryCode', country);
   }
 }
