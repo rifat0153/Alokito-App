b3bacfa3f34c1d864286dddc208862ad9a39e68e
Mahbubur Rahman <rifat0153@gmail.com>
2021-08-23 19:55:43 +0600

    Gift show in veiw INIT

--- lib/modules/gift_receiver/services/gift_receiver_service.dart
+++ lib/modules/gift_receiver/services/gift_receiver_service.dart
@@ -1,8 +1,13 @@
+import 'dart:convert';
+
+import 'package:alokito_new/models/gift_giver/gift_giver.dart';
 import 'package:alokito_new/modules/gift_receiver/gift_receiver_exception.dart';
 import 'package:alokito_new/models/gift_giver/gift_receiver.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:geoflutterfire/geoflutterfire.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 abstract class BaseGiftReceiverService {
   Future<bool> addGiftRequest({required GiftReceiver giftReceiver});
@@ -21,6 +26,33 @@ class GiftReceiverService implements BaseGiftReceiverService {
 
   final Geoflutterfire geo;
   final FirebaseFirestore _firestore;
+
+ @override
+  Future<GiftGiverListUnion> getGiftDB(String page, String limit, LatLng latLng, double radius) async {
+    final client = http.Client();
+
+    final http.Response response = await client.get(
+      Uri.parse('http://192.168.0.121:3000/api/v1/gift/near?lat=23&lng=91&maxDistance=104&page=1&limit=5'),
+      headers: {"Content-Type": "application/json"},
+    );
+
+    final Map<String, dynamic> body = jsonDecode(response.body) as Map<String, dynamic>;
+
+    final List<dynamic> giftJson = body['gifts'] as List<dynamic>;
+
+    final List<GiftGiver> gifts = giftJson
+        .map(
+          (user) => GiftGiver.fromJson(giftJson as Map<String, dynamic>),
+        )
+        .toList();
+
+    gifts.forEach((element) {
+      print(element);
+    });
+
+    return GiftGiverListUnion.data(gifts);
+  }
+
 
   @override
   Future<GiftReceiverNotificationUnion> getGiftRequest({required String id}) async {
