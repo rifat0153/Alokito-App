bace937ca5616880c1bb52e2b1391f7a69ecd971
Mahbubur Rahman <rifat0153@gmail.com>
2022-07-02 17:00:57 +0100

    BaseURL updated

--- lib/shared/config.dart
+++ lib/shared/config.dart
@@ -2,15 +2,15 @@ import '../models/user/local_user.dart';
 import 'package:flutter/material.dart';
 
 class MyConfig {
-  static const baseUrl = 'https://aalokito-dev-0-11-jyguqmmqea-uc.a.run.app/api/v1';
+  static const baseUrl = 'https://aalokito-v-dev-13-jyguqmmqea-uc.a.run.app/api/v1';
 // static const baseUrl = 'http://192.168.118.176:80/api/v1';
 
-  static const myTimeout = 15;
+  static const myTimeout = 30;
 }
 
 // URL
-const baseUrl = 'http://192.168.0.215:80/api/v1';
-// const baseUrl = 'https://aalokito-dev-0-11-jyguqmmqea-uc.a.run.app/api/v1';
+const baseUrl = 'https://aalokito-v-dev-13-jyguqmmqea-uc.a.run.app/api/v1';
+// const baseUrl = 'http://192.168.0.215:80/api/v1';
 
 const myTimeout = 8;
 const imageAssetPath = 'assets/images/';
